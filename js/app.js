39db2845c9283fa92bb5bd7901e329a568b5c537
Lucky7KP <kpeltier@Kristinas-MacBook-Pro.local>
2014-08-11 00:11:18 -0700

    setup html and questions

---
+++ js/app.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+
+	//Quotes & Answers
+	var questions = [
+		{
+			number: "first-question",
+			quote: "Surely you can't be serious.  I am serious.  And don't call me Shirley."
+			answers: ["Dumb and Dumber", "Austin Powers", "Airplane", "Office Space"],
+			correct: 1,
+			correction: "Airplane is the correct movie.  The line became a trademark for Leslie Nielsen, much to his own surpirse.",
+			confirm:  "This was so popular in the 80's it is number 79 on the AFI list of favorite movie quotes.",
+		),
+		{
+			number: "second-question",
+			quote: "You keep using that word.  I don't think it means what you think it means."
+			answers: ["The Adventures fo Baron Munchausen", "One Flew Over the Cuckoo's Nest", "Wayne's World", "The Princess Bride"],
+			correct: 3,
+			correction: "Sorry, wrong movie.  Inigo Montoya said this in the Princess Bride.",
+			confirm:  "A well know phrase from the Princess Bride, but not as popular as Inigo other famous line: Hello! My name is Inigo Montoya  You killed my father prepare to die.",
+		),
+		{
+			number: "third-question",
+			quote: "The greatest trick the devil ever pulled was convincing the world he didn’t exist.  And like that he’s gone."
+			answers: ["The Usual Suspects", "The Exorcist", "Omen", "The Shining"],
+			correct: 0,
+			correction: "Airplane is the correct movie.  The line became a trademark for Leslie Nielsen, much to his own surpirse.",
+			confirm:  "This was so popular in the 80's it is number 79 on the AFI list of favorite movie quotes.",
+		),
+		{
+			number: "fourth-question",
+			quote: "Whoa! Whoa! Whoa! Okay, you are now firing a gun at your imaginary friend near 400 gallons of nitroglycerine!"
+			answers: ["The Expendables", "Die Hard", "Fight Club", "Raiders of the Lost Ark"],
+			correct: 2,
+			correction: "Airplane is the correct movie.  The line became a trademark for Leslie Nielsen, much to his own surpirse.",
+			confirm:  "This was so popular in the 80's it is number 79 on the AFI list of favorite movie quotes.",
+		),
+		{
+			number: "fifth-question",
+			quote: "Why you stuck-up, half-witted, scruffy-looking nerf-herder!"
+			answers: ["The Shootist", "Star Wars", "Serenity", "Office Space"],
+			correct: 1,
+			correction: "Airplane is the correct movie.  The line became a trademark for Leslie Nielsen, much to his own surpirse.",
+			confirm:  "This was so popular in the 80's it is number 79 on the AFI list of favorite movie quotes.",
+		)
+	];
+
+
+	]
+
+
+};
